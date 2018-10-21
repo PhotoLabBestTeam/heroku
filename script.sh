5aa1ff1471eb0d96d32e05e1d6c0774a31a43c3a
Maria.Feofanova <maria.feofanova@jetbrains.com>
2018-10-21 16:36:03 +0300

    remove pwd

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ img_url=$(curl -v -X POST "http://upload-hack.photolab.me/upload.php" -F file1=@
 out_url=$(curl -v -X POST "http://api-hack.photolab.me/template_process.php" -F image_url[1]=${img_url} -F template_name="E7136FD5-4D98-9354-0184-B940EBAC0ACB")
 echo $out_url > out.txt
 curl $out_url > "static/middle.jpg"
-curr_dir=$(pwd)"/static/middle.jpg"
+curr_dir="/static/middle.jpg"
 
-python post_processing/main.py $curr_dir $(pwd)"/static/result.jpg"
+python post_processing/main.py $curr_dir "/static/result.jpg"
 
-echo $(pwd)"/static/result.jpg" > out.txt
+echo "/static/result.jpg" > out.txt
